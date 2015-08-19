61adc13db899329bee5fdf779da9f0eeff1b504e
Salih KARAHAN <salihkarahan@outlook.com>
2015-08-19 22:06:57 +0300

    Added new enums and functions

--- src/talented-ui.core.js
+++ src/talented-ui.core.js
@@ -2,11 +2,6 @@
     'use strict';
 
     (function() {
-        if (window.log) {
-            window.log = function(message) {
-
-            };
-        }
 
         if (!window.console) {
             window.console = {
@@ -17,15 +12,66 @@
                 error: function() {}
             };
         }
+
     })();
 
     var _talentedUi = function() {
 
+        this.Events = {
+            Click: 'click',
+            ContextMenu: 'contextmenu'
+        };
+
+        this.Types = {
+            String: 'String',
+            HTMLDivElement: 'HTMLDivElement'
+        };
+
         this.Elements = {
+            A: 'a',
+            Button: 'button',
+            Input: 'input',
+            Select: 'select',
+            Option: 'option',
+            Tr: 'tr',
+            Th: 'th',
+            Td: 'td',
             Table: 'table',
-            TableHead: 'thead',
-            TableBody: 'tbody',
-            TableFoot: 'tfoot'
+            THead: 'thead',
+            TBody: 'tbody',
+            TFoot: 'tfoot',
+            Ul: 'ul',
+            Ol: 'ol',
+            Li: 'li'
+        };
+
+        this.RegisterEvent = function(element, eventName, callback) {
+            if (element.addEventListener) {
+                element.addEventListener(eventName, callback, false);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + eventName, callback);
+            } else if (element['on' + eventName]) {
+                element['on' + eventName] = callback;
+            } else {
+                throw console.error('');
+            }
+        }
+
+        this.ReleaseEvent = function(element, eventName, callback) {
+            if (element.removeEventListener) {
+                element.removeEventListener(eventName, callback);
+            } else if (element.detachEvent) {
+                element.detachEvent('on' + eventName, callback);
+            } else if (element['on' + eventName]) {
+                element['on' + eventName] = undefined;
+            } else {
+                throw console.error('');
+            }
+        }
+
+        // TODO:
+        this.CreateCustomEvent = function(argument) {
+
         };
 
         this.GetType = function(obj) {
@@ -41,6 +87,23 @@
             var createdElement = document.createElement(elementName);
             return createdElement;
         };
+
+        this.SetAttribute = function (element, attributeName, attributeValue) {
+            if (!element.hasAttribute(attributeName)) {
+                console.log(attributeValue);
+                element.setAttribute(attributeName,attributeValue);
+            }
+        };
+
+        this.UpdateAttribute = function (element, attributeName, attributeValue) {
+
+        };
+
+        this.RemoveAttribute = function (element, attributeName) {
+            if (element.hasAttribute(attributeName)) {
+                element.removeAttribute(attributeName);
+            }
+        }
 
     };
 
